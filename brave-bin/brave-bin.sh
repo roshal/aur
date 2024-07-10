c7565badf74d112878e874c95672d587913016ce
roshal <roshal@users.noreply.github.com>
2024-07-10 10:45:35 +0300

    update brave-bin to use mapfile

--- brave-bin/brave-bin.sh
+++ brave-bin/brave-bin.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
-# Allow users to override command-line options
-USER_FLAGS_FILE="$XDG_CONFIG_HOME/brave-flags.conf"
-if [[ -f $USER_FLAGS_FILE ]]; then
-   USER_FLAGS="$(cat $USER_FLAGS_FILE | sed 's/#.*//')"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
+
+USER_FLAGS_FILE="${XDG_CONFIG_HOME}/brave-flags.conf"
+
+declare -a USER_FLAGS
+
+if
+    test -f "${USER_FLAGS_FILE}"
+then
+    mapfile -t USER_FLAGS_MAPFILE < "${USER_FLAGS_FILE}"
 fi
+
+for USER_FLAGS_LINE in "${USER_FLAGS_MAPFILE[@]}"
+do
+    if ! [[ "${USER_FLAGS_LINE}" =~ ^[[:space:]]*(#|$) ]]
+    then
+        USER_FLAGS+=("${USER_FLAGS_LINE}")
+    fi
+done
 
 export CHROME_VERSION_EXTRA="stable"
 
-exec /opt/brave-bin/brave "$@" $USER_FLAGS
+exec /opt/brave-bin/brave "${USER_FLAGS[@]}" "${@}"
